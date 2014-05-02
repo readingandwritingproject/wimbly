eae39314aabd188887e726bd4a4390b82bd890a1
Reading and Writing Project <admin@readingandwritingproject.com>
2014-05-02 10:35:55 -0400

    Added redis to the mix

--- start.sh
+++ start.sh
@@ -8,5 +8,16 @@ sudo $SCRIPT_DIR/nginx/sbin/nginx -t -c $SCRIPT_DIR/nginx/conf/nginx.conf
 CONFIG_TEST_RESULT=$?
 if [ $CONFIG_TEST_RESULT -eq 0 ]; then
   sudo $SCRIPT_DIR/nginx/sbin/nginx -c $SCRIPT_DIR/nginx/conf/nginx.conf
-  echo '... started'
+  if [ $? -eq 0 ]; then
+    echo '... started'
+  fi
+fi
+
+if [ -f $SCRIPT_DIR/redis/bin/redis-server ]
+then
+  echo Redis start
+  $SCRIPT_DIR/redis/bin/redis-server $SCRIPT_DIR/redis/conf/redis.conf
+  if [ $? -eq 0 ]; then
+    echo '... started'
+  fi
 fi
--- stop.sh
+++ stop.sh
@@ -9,3 +9,13 @@ CONFIG_TEST_RESULT=$?
 if [ $CONFIG_TEST_RESULT -eq 0 ]; then
   echo '... stopped'
 fi
+
+if [ -f $SCRIPT_DIR/redis/bin/redis-server ]
+then
+  echo Redis stop
+  $SCRIPT_DIR/redis/bin/redis-cli SHUTDOWN
+  if [ $? -eq 0 ]; then
+    echo '... stopped'
+  fi
+fi
+
