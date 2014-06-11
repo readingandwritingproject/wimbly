0ed80b0b1266f3c0cba489875cc6853f2a052225
Reading and Writing Project <admin@readingandwritingproject.com>
2014-06-11 10:59:58 -0400

    Put init into a file

---
+++ install/openresty/wimbly_init.lua
@@ -0,0 +1,50 @@
+
+-- part of openresty
+cjson = require "cjson"
+
+-- luarocks
+require "luarocks.loader"
+
+-- uuid generator
+uuid = require "uuid"
+uuid.randomseed( ngx.now() * 10000 )
+
+-- bcrypt password hasher
+bcrypt = require "bcrypt"
+
+-- date library
+date = require "date"
+
+
+-- wrapping some of podofo
+pdf = require "libpdf"
+
+-- wrapping some of libcsv in an interface like SLAXML
+slacsv = require "libcsv"
+
+-- for data structure inspection during development
+inspect = require "inspect"
+
+
+-- for light-weight classes
+class = require "middleclass"
+
+-- for a streaming XML parser
+slaxml = require "slaxml"
+
+
+-- for some utility functions
+require "util"
+
+-- form and field validation library
+validate = require "validate"
+
+-- REST API helper
+restfully = require "restfully"
+
+wimbly = require "wimbly"
+
+-- preprocess connect application conf 
+wimbly.preprocess( "/var/www/application", {
+  ["app"] = "/var/www/application"
+} )
