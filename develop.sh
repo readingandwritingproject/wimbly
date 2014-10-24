bb2e4079d264341b1ce62c46f9ed00e146a99628
Daniel Rubin <daniel@rubin.za.net>
2014-10-24 13:59:42 +0000

    More general file watching enabled

--- develop.sh
+++ develop.sh
@@ -5,11 +5,10 @@ cd $SCRIPT_DIR
 
 while true
 do
-  inotifywait -e modify $SCRIPT_DIR/nginx/conf/* `find application -name '*.lua' | xargs -n 1` `find application -name '*.conf.source' | xargs -n 1`
+  inotifywait -e modify $SCRIPT_DIR/nginx/conf/* `find . -name '*.lua' | xargs -n 1` `find application -name '*.conf.source' | xargs -n 1`
   $SCRIPT_DIR/reload.sh
   if [ $? -eq 0 ]
   then
     echo "(Reloading Nginx)"
   fi
 done
-                
